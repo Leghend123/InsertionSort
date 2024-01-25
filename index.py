ba3cafbbfa6672bf08016147af0b4c47e69e32ef
nelson <nelsonagbagah1002@gmail.com>
2024-01-25 03:54:31 -0800

    updated index.py

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 #Insertion sorting algorithm
 
+#function to do the sorting
 def insert_sort(arr):
     #lenght of the array..............
     len_arr = len(arr)
+    #tranvese through all lenght of the array
     for i in range(len_arr):
         a = arr[i]
         j =i-1
+        #loop through the array
         while j>=0 and a<arr[j]:
             arr[j+1] = arr[j]
             j -=1
